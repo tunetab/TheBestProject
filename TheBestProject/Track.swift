ac770875574917badbcb0c743798cf46f8a231dd
tunetab <s.panov@g.nsu.ru>
2022-01-28 16:34:43 +0700

    setup app remote

---
+++ TheBestProject/Track.swift
@@ -0,0 +1,19 @@
+//
+//  Track.swift
+//  TheBestProject
+//
+//  Created by Стажер on 28.01.2022.
+//
+
+import Foundation
+import UIKit
+
+struct Track {
+    let name: String
+    let artist: String
+    let genre: String
+    let coverImage: UIImage
+    let album: String
+    let year: Date
+    let url: URL
+}
