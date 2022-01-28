9ed8c1f10c1b08b68c0b27711c27353d9c3cb4fb
tunetab <s.panov@g.nsu.ru>
2022-01-28 12:17:37 +0700

    Created viewControllers

---
+++ TheBestProject/ViewControllers/ItemViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ItemViewController.swift
+//  TheBestProject
+//
+//  Created by Стажер on 28.01.2022.
+//
+
+import UIKit
+
+class ItemViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
